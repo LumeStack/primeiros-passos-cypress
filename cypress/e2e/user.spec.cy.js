9295b922f090d2ced7f00984a02ef53bc9353956
henriquemanieri <henrique.manieri@hotmail.com>
2023-06-12 19:00:56 -0400

    add ramdon values on user spec chancejs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -2,28 +2,19 @@ import userData from '../fixtures/users/userData.json'
 import LoginPage from '../pages/loginPage'
 import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
+import MyInfoPage from '../pages/myInfoPage'
 
+const Chance = require('chance')
+
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
+const myInfoPage = new MyInfoPage()
 
 describe('Orange HRM Tests', () => {
 
-  const selectorsList = {
-
-    firstNameField: "[name='firstName']",
-    lastNameField: "[name='lastName']",
-    genericField: ".oxd-input--active",
-    dateField: "[placeholder='yyyy-mm-dd']",
-    genericCombobox: ".oxd-select-text--arrow",
-    secondItemCombobox: ".oxd-select-dropdown > :nth-child(2)",
-    thirdItemCombobox: ".oxd-select-dropdown > :nth-child(3)",
-    dateCloseButton: ".--close",
-    submitButton: ".orangehrm-left-space"
-
-  }
-
-  it.only('User Info Update - Success', () => {
+  it('User Info Update - Success', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
 
@@ -31,31 +22,11 @@ describe('Orange HRM Tests', () => {
 
     menuPage.accessMyInfo()
 
-    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
-    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
-    cy.get(selectorsList.genericField).eq(3).clear().type('NicknameTest')
-    cy.get(selectorsList.genericField).eq(4).clear().type('Employee')
-    cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
-    cy.get(selectorsList.genericField).eq(6).clear().type('DriversLicenseTest')
-    cy.get(selectorsList.genericField).eq(7).clear().type('2025-03-10')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.genericField).eq(8).clear().type('ssnNumberTest')
-    cy.get(selectorsList.genericField).eq(9).clear().type('sinNumberTest')
-    cy.get(selectorsList.submitButton).eq(0).click({ force: true })
-    cy.get('body').should('contain', 'Successfully Updated')
-    cy.get('.oxd-toast-close')
-
-    cy.get(selectorsList.genericCombobox).eq(0).click({ force: true })
-    cy.get(selectorsList.secondItemCombobox).click()
-    cy.get(selectorsList.genericCombobox).eq(1).click({ force: true })
-    cy.get(selectorsList.thirdItemCombobox).click()
-
+    myInfoPage.fillPersonalDetails(chance.first(), chance.last(), chance.string())
+    myInfoPage.fillEmployeeDetails('EmployId', 'otherId', 'Drivers Number', '2025-07-29', '123456', '0987654')
+    myInfoPage.fillStatus()
+    myInfoPage.saveForm()
   })
-  it('Login - Fail', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userFail.username)
-    cy.get(selectorsList.passwordField).type(userData.userFail.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.get(selectorsList.wrongCredentialAlert)
-  })
+
+
 })
--- cypress/pages/menuPage.js
+++ cypress/pages/menuPage.js
@@ -17,6 +17,7 @@ class MenuPage {
         cy.get(this.selectorsList().performanceButton).click()
     }
 
+
 }
 
 export default MenuPage
